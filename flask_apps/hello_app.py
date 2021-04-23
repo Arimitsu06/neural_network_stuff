408634b677e065e6dd94cc483c013b8c4bf192de
Ari <arimitsu0803@gmail.com>
2021-04-23 13:26:48 -0400

    Front-end Flask stuff.

--- flask_apps/hello_app.py
+++ flask_apps/hello_app.py
@@ -1,14 +1,19 @@
-from flask import request
-from flask import jsonify
 from flask import Flask
+from flask import jsonify
+from flask import request
+import json as json
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/hello', methods=['POST'])
+@app.route('/hello',methods=['POST'])
+@cross_origin()
 def hello():
     message = request.get_json(force=True)
     name = message['name']
     response = {
-        'greeting' : 'Hello, ' + name + '!'
+        'greeting': 'Hello, ' + name + '!'
     }
     return jsonify(response)
